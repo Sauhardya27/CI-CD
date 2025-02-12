a6aa31b983e06b66731e333338fa765f8e7d8086
Sauhardya27 <sauhardyachakraborty2643@gmail.com>
2025-02-12 13:05:02 +0530

    New PR

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -3,6 +3,7 @@ import { redirect } from 'next/navigation'
 import { authOptions } from "./lib/auth";
 
 //Added some comments
+//Added more comments
 
 export default async function Page() {
   const session = await getServerSession(authOptions);
